dbcd80026713ea5c9eb3a21e051eec0c54dc1489
DevelopJKong <ybaram40@gmail.com>
2022-02-03 07:19:36 +0900

    레이아웃 완성 02.03

--- src/init.js
+++ src/init.js
@@ -2,11 +2,19 @@ import express from "express";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import boardRouter from "./routers/boardRouter";
+import morgan from "morgan";
 
 const app = express();
 const PORT = 8000;
+const logger = morgan("dev");
+
+
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
+
+app.use(logger);
+app.use("/image",express.static(__dirname + "/image"));
+app.use("/static",express.static(__dirname + "/assets"));
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/board", boardRouter);
