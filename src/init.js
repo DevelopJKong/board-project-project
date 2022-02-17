3dcca0bd4d2cff832432498eac4efea91c4916b8
DevelopJKong <ybaram40@gmail.com>
2022-02-17 18:27:07 +0900

    heroku 배포를 위해 PORT 설정 완료

--- src/init.js
+++ src/init.js
@@ -5,7 +5,7 @@ import "./models/Board";
 import "./models/User";
 import app from "./server";
 
-const PORT = 5050;
+const PORT = process.env.PORT || 5050;
 
 const handleListener = () => {
   console.log(`Hello! let's start http://localhost:${PORT}`);
